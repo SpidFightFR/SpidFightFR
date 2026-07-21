934d3b77a8ee929d266a2d4ac74f6004e0b902d1
SpidFightFR <spidfight@swisscows.email>
2026-07-21 19:45:52 +0200

    Refactoring: install-software.ps1

--- scripts/powershell/winget/install-software.ps1
+++ scripts/powershell/winget/install-software.ps1
@@ -1,29 +1,80 @@
 #Var for current folder
-$currentDir = (pwd).Path
+$currentDir = $PSScriptRoot
+
+$srcDir = "${currentDir}\src"
+$tmpDir = "${srcDir}\tmp"
+
+$repo = "microsoft/winget-cli"
+$releaseLatest = Invoke-RestMethod -Uri "https://api.github.com/repos/$repo/releases/latest"
+$versionLatest = $releaseLatest.tag_name
+
+$assets = $releaseLatest.assets
+
+Write-Host "Latest release tag: $versionLatest"
+
+
+$dirsToCreate = @($srcDir, $tmpDir)
+
+
+#################################
+# CLEANING UP PREVIOUS INSTALLS #
+#################################
+
+
+# Clean previous installs
+if (Test-Path -Path "${srcDir}" -PathType Container){
+    Write-Host "Clearing previous sources dir..."
+	Remove-Item -Recurse -Force $srcDir
+    Write-Host "Sources dir cleared."
+}
+
+foreach ($dir in $dirsToCreate){
+	New-Item -ItemType Directory $dir
+    Write-Host "Created new sources directory: $dir"
+}
+
+
+############################
+# DOWNLOADING DEPENDENCIES #
+############################
+
+
 
 #Download dependencies
-(New-Object System.Net.WebClient).DownloadFile('https://github.com/microsoft/winget-cli/releases/download/v1.29.280/DesktopAppInstaller_Dependencies.zip',"$currentDir\Dependencies.zip")
-Expand-Archive "Dependencies.zip"; rm "Dependencies.zip"
-mv "Dependencies\x64\Microsoft.VCLibs.140.00_14.0.33519.0_x64.appx" "$currentDir\VCLibs.appx"
-mv "Dependencies\x64\Microsoft.VCLibs.140.00.UWPDesktop_14.0.33728.0_x64.appx" "$currentDir\VCLibs_UWPDesktop.appx"
-mv "Dependencies\x64\Microsoft.WindowsAppRuntime.1.8_8000.616.304.0_x64.appx" "$currentDir\Xaml.appx"
-Remove-Item -Recurse -Force ".\Dependencies"
+(New-Object System.Net.WebClient).DownloadFile("$($assets[3].browser_download_url)","$tmpDir\Dependencies.zip")
+Expand-Archive "${tmpDir}\Dependencies.zip" -DestinationPath "${tmpDir}\";
+mv "${tmpDir}\x64\Microsoft.VCLibs.*.UWPDesktop_*_x64.appx" "$srcDir\VCLibs_UWPDesktop.appx"
+mv "${tmpDir}\x64\Microsoft.VCLibs.*_x64.appx" "$srcDir\VCLibs.appx"
+mv "${tmpDir}\x64\Microsoft.WindowsAppRuntime.*_x64.appx" "$srcDir\Xaml.appx"
 
 
-(New-Object System.Net.WebClient).DownloadFile('https://github.com/microsoft/winget-cli/releases/download/v1.29.280/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle',"$currentDir\Winget.msixbundle")
 
-(New-Object System.Net.WebClient).DownloadFile('https://github.com/microsoft/winget-cli/releases/download/v1.29.280/e53e159d00e04f729cc2180cffd1c02e_License1.xml',"$currentDir\License.xml")
+#Winget msix download
+(New-Object System.Net.WebClient).DownloadFile("$($assets[5].browser_download_url)","$srcDir\Winget.msixbundle")
 
+#License download
+(New-Object System.Net.WebClient).DownloadFile("$($assets[4].browser_download_url)","$srcDir\License.xml")
 
-(New-Object System.Net.WebClient).DownloadFile('https://cdn.winget.microsoft.com/cache/source.msix',"$currentDir\Sources.msix")
+#Sources Download
+(New-Object System.Net.WebClient).DownloadFile('https://cdn.winget.microsoft.com/cache/source.msix',"$srcDir\Sources.msix")
+
+try{
+    Remove-Item -Recurse -Force $tmpDir
+    Write-Host "Temp dir removed."
+}catch {
+    Write-Error "Error while removing the temp dir: $($_.Exception.Message)"
+}
+
+################################
+# INSTALLING APPS USING WINGET #
+################################
 
 #install winget
-Add-AppxPackage -Path ".\VCLibs.appx";
-Add-AppxPackage -Path ".\VCLibs_UWPDesktop.appx";
-Add-AppxPackage -Path ".\Xaml.appx";
-Add-AppxPackage -Path ".\Winget.msixbundle";
-Add-AppxProvisionedPackage -Online -PackagePath ".\Winget.msixbundle" -LicensePath ".\License.xml";
-
+Add-AppxPackage -Path "${srcDir}\VCLibs.appx";
+Add-AppxPackage -Path "${srcDir}\VCLibs_UWPDesktop.appx";
+Add-AppxPackage -Path "${srcDir}\Xaml.appx";
+Add-AppxPackage -Path "${srcDir}\Winget.msixbundle";
+Add-AppxProvisionedPackage -Online -PackagePath "${srcDir}\Winget.msixbundle" -LicensePath "${srcDir}\License.xml";
 
 #reset winget sources plus src tweaks
 winget source reset --force;
@@ -31,10 +82,13 @@ winget source remove msstore;
 winget update all --force;
 
 #Add sources manually
-Add-AppPackage -path ".\Sources.msix";
+Add-AppPackage -path "${srcDir}\Sources.msix";
 
 #Updating the existing system
 winget upgrade --all --accept-package-agreements --accept-source-agreements;
 
-#Installing base softwares
-winget import --accept-package-agreements --accept-source-agreements -i ".\Applist.json";
+#Installing softwares
+winget import --accept-package-agreements --accept-source-agreements -i "${currentDir}\Applist.json"
+
+#Restricting back script policy
+Set-ExecutionPolicy Restricted;
